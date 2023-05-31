0da7668ca7261bd93c7261b5adfbd5036e5f35e5
christianarca27 <christian.arca.mail@gmail.com>
2023-05-31 16:16:50 +0200

    feat: aggiunta chiamata API per mostrare il singolo progetto + ordinati in modo descrescente i risultati della query di index

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,11 +11,27 @@ class ProjectController extends Controller
     public function index()
     {
         // $projects = Project::all();
-        $projects = Project::with('type', 'technologies')->get();
+        $projects = Project::with('type', 'technologies')->orderByDesc('created_at')->get();
 
         return response()->json([
             'success' => true,
             'results' => $projects
         ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->first();
+
+        if (!empty($project)) {
+            return response()->json([
+                'success' => true,
+                'results' => $project,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+            ]);
+        }
     }
 }
