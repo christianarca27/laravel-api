413dc953118db7b5fa56c7073f1ae59a134c3d41
christianarca27 <christian.arca.mail@gmail.com>
2023-05-31 17:44:14 +0200

    feat: aggiunto parametro alla index dei progetti per consentire di filtrare per Tipo nel caso fosse indicato nella richiesta

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,23 +4,30 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 use function PHPUnit\Framework\isEmpty;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // $projects = Project::all();
-        $projects = Project::with('type', 'technologies')->orderByDesc('created_at')->get();
+        if ($request->has('type_id')) {
+            $queryParams = $request->all();
 
-        // dd($projects);
+            $projects = Project::with('type', 'technologies')->where('type_id', $queryParams['type_id'])->orderByDesc('created_at')->get();
+        } else {
+            $projects = Project::with('type', 'technologies')->orderByDesc('created_at')->get();
+        }
+
+        $types = Type::all();
 
         if (count($projects) > 0) {
             return response()->json([
                 'success' => true,
                 'results' => $projects,
+                'types' => $types,
             ]);
         } else {
             return response()->json([
